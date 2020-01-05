65532d54d9a14afe7cb8283dc53ddc1af2b4d374
nosyarg <york17a@ncssm.edu>
2020-01-05 14:27:26 -0500

    removed debug code from Dr. F's exercise

--- questions/quadratic-pattern.py
+++ questions/quadratic-pattern.py
@@ -2,20 +2,16 @@ from sympy import*
 import random
 def gettext(inseed):
     p,q,e = gendata(inseed)
-    print((p,q,e))
     x = Symbol('x')
     expr = (x+p)*(x+q)
     expr = expr.subs(x, x**e)
     return "Completely factor the following: \\(" + latex(expand(expr)) + "\\)"
 def checkanswer(inseed, user_answer):
     p,q,e = gendata(inseed)
-    print((p,q,e))
     x = Symbol('x')
     user_answer = sympify(user_answer)
     expr = (x**e+p)*(x**e+q)
     answer = factor(expr)
-    #print(expr)
-    #print(user_answer)
     if answer == user_answer:
       return True
     else:
